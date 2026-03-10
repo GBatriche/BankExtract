d0f2571706e731c069c7a04d9748e93dc728f5a9
GBatriche <batricheg@gmail.com>
2026-03-10 08:43:22 -0500

    refactored using the method parseLines

--- src/BankTransactionAnalyzerSimple.java
+++ src/BankTransactionAnalyzerSimple.java
@@ -11,18 +11,15 @@ public class BankTransactionAnalyzerSimple {
     private static final String RESOURCES = "src/main/resources/";
 
     public static void main (final String... args) throws IOException {
-        final Path path = Paths.get(RESOURCES + args[0]);
+        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
-        double total = 0d;
-        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        for (final String line: lines) {
-            final String[] columns = line.split(",");
-            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-            if (date.getMonth() == Month.JANUARY) {
-                final double amount = Double.parseDouble(columns[1]);
-                total += amount;
-            }
-        }
-        System.out.println("The total for all transactions in January is " + total);
+
+        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in January " + selectInMonth(BankTransaction, Month.JANUARY));
     }
 }
